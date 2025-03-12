13f982a5fbdfde12e2bfda6c9f4b1e184191dacb
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-03-12 19:36:15 +0600

    Enhance Home page with Ant Design components and typography elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,45 @@
-import Buttons from "@/components/Buttons";
+"use client";
+import { FloatButton, Space, Typography, Divider } from "antd";
 
-import { FloatButton } from "antd";
+const { Title, Paragraph, Text, Link } = Typography;
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Buttons />
-
         <FloatButton />
+
+        <Title>This is a test page for the Next.js + Tailwind CSS + Ant Design setup.</Title>
+        <Title level={2}>This is a test page for the Next.js + Tailwind CSS + Ant Design setup.</Title>
+        <Title level={3}>This is a test page for the Next.js + Tailwind CSS + Ant Design setup.</Title>
+        <Title level={4}>This is a test page for the Next.js + Tailwind CSS + Ant Design setup.</Title>
+        <Title level={5}>This is a test page for the Next.js + Tailwind CSS + Ant Design setup.</Title>
+
+        <Paragraph>This is a test page for the Next.js + Tailwind CSS + Ant Design setup.</Paragraph>
+
+        <Text>This is a test page for the Next.js + Tailwind CSS + Ant Design setup.</Text>
+
+        <Link href="">This is a test page for the Next.js + Tailwind CSS + Ant Design setup.</Link>
+
+        <Space direction="vertical">
+          <Text>Ant Design (default)</Text>
+          <Text type="secondary">Ant Design (secondary)</Text>
+          <Text type="success">Ant Design (success)</Text>
+          <Text type="warning">Ant Design (warning)</Text>
+          <Text type="danger">Ant Design (danger)</Text>
+          <Text disabled>Ant Design (disabled)</Text>
+          <Text mark>Ant Design (mark)</Text>
+          <Text code>Ant Design (code)</Text>
+          <Text keyboard>Ant Design (keyboard)</Text>
+          <Text underline>Ant Design (underline)</Text>
+          <Text delete>Ant Design (delete)</Text>
+          <Text strong>Ant Design (strong)</Text>
+          <Divider />
+          <Text italic>Ant Design (italic)</Text>
+          <Link href="https://ant.design" target="_blank">
+            Ant Design (Link)
+          </Link>{" "}
+        </Space>
       </main>
     </div>
   );
